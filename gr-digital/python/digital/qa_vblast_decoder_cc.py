bb430066d334c223c48cb4b5c2a42972c29a3d09
luca <luca.moritz.schmid@gmail.com>
2018-06-19 11:13:08 +0200

    VBLAST: Add qa test for 2x2 MMSE case.

--- gr-digital/python/digital/qa_vblast_decoder_cc.py
+++ gr-digital/python/digital/qa_vblast_decoder_cc.py
@@ -35,9 +35,17 @@ class qa_vblast_decoder_cc (gr_unittest.TestCase):
     def tearDown (self):
         self.tb = None
 
-    def dice_csi_tags(self, data, num_inputs, num_tags, tag_pos):
+    def dice_csi_tags(self, data, type, num_inputs, num_tags, tag_pos):
         tags = []
         expected_result = np.empty([np.size(data, 0)*np.size(data,1)], dtype=complex)
+
+        if type == 'MMSE':
+            # Add an SNR tag at the start of the stream for MMSE.
+            tags.append(gr.tag_utils.python_to_tag((0,
+                                                    pmt.string_to_symbol("snr"),
+                                                    pmt.make_f32vector(num_inputs, 1e8),
+                                                    pmt.from_long(0))))
+
         for i in range(0, num_tags):
             # Randomly generate CSI for one symbol.
             csi = (np.random.randn(num_inputs, num_inputs) + 1j * np.random.randn(num_inputs, num_inputs))
@@ -79,6 +87,7 @@ class qa_vblast_decoder_cc (gr_unittest.TestCase):
             tag_pos = np.sort(tag_pos)
             # Calculate expected result.
             tags, expected_result = self.dice_csi_tags(data,
+                                                       'ZF',
                                                        num_inputs,
                                                        num_tags,
                                                        tag_pos)
@@ -101,6 +110,50 @@ class qa_vblast_decoder_cc (gr_unittest.TestCase):
 
             # Check if the expected result equals the actual result.
             self.assertComplexTuplesAlmostEqual(expected_result, sink.data(), 4)
+
+    ''' 
+    5 tests validating the correct output of the decoder with random input data, MMSE equalizer
+    and 2x2 MIMO scheme and an extremely high SNR regime (1/snr -> 0). '''
+    def test_002_t(self):
+        # Define test params.
+        data_length = 20
+        repetitions = 5
+        num_tags = 4
+        num_inputs = 2
+        equalizer_type = 'MMSE'
+
+        for i in range(repetitions):
+            # Generate random input data.
+            data = np.random.randn(num_inputs, data_length) + 1j * np.random.randn(num_inputs, data_length)
+            # Generate random tag positions.
+            tag_pos = np.random.randint(low=0, high=data_length, size=num_tags)
+            tag_pos[0] = 0
+            tag_pos = np.sort(tag_pos)
+            # Calculate expected result.
+            tags, expected_result = self.dice_csi_tags(data,
+                                                       'MMSE',
+                                                       num_inputs,
+                                                       num_tags,
+                                                       tag_pos)
+
+            # Build up the test flowgraph.
+            src = []
+            src.append(blocks.vector_source_c(data=data[0],
+                                            repeat=False,
+                                            tags=tags))
+            for n in range(1, num_inputs):
+                src.append(blocks.vector_source_c(data=data[n],
+                                                  repeat=False))
+            vblast_decoder = digital.vblast_decoder_cc(num_inputs, equalizer_type)
+            sink = blocks.vector_sink_c()
+            self.tb.connect(src[0], vblast_decoder, sink)
+            for n in range(1, num_inputs):
+                self.tb.connect(src[n], (vblast_decoder, n))
+            # Run flowgraph.
+            self.tb.run()
+
+            # Check if the expected result equals the actual result.
+            self.assertComplexTuplesAlmostEqual(expected_result, sink.data(), 2)
 
 
 if __name__ == '__main__':
