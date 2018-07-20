138456c9bc054b832429de29246f7447dde5a4c2
luca <luca.moritz.schmid@gmail.com>
2018-07-20 09:08:37 +0200

    ofdm_mimo_channel_est: Add qa test with ofdm_tx src.

--- gr-digital/python/digital/qa_mimo_ofdm_channel_estimator_vcvc.py
+++ gr-digital/python/digital/qa_mimo_ofdm_channel_estimator_vcvc.py
@@ -23,7 +23,10 @@
 
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
+from gnuradio import fft
 import digital_swig as digital
+import numpy as np
+import pmt
 
 class qa_mimo_ofdm_channel_estimator_vcvc (gr_unittest.TestCase):
 
@@ -34,7 +37,35 @@ class qa_mimo_ofdm_channel_estimator_vcvc (gr_unittest.TestCase):
         self.tb = None
 
     def test_001_t (self):
-        # set up fg
+        # Define test params.
+        packet_len = 50
+        len_tag_key = packet_len
+        fft_len = 64
+        N=2
+        M=2
+        channel_matrix = (np.random.randn(N, M) + 1j * np.random.randn(N, M))
+
+        src = blocks.vector_source_b(range(packet_len), True, 1, ())
+        s2tagged_stream = blocks.stream_to_tagged_stream(gr.sizeof_char, 1,
+                                                                               packet_len,
+                                                                               len_tag_key)
+        ofdm_tx = digital.ofdm_tx(
+            fft_len=fft_len, cp_len=fft_len / 4,
+            packet_length_tag_key=len_tag_key,
+            bps_header=1,
+            bps_payload=2,
+            rolloff=0,
+            debug_log=False,
+            scramble_bits=False,
+            m=2, mimo_technique="vblast"
+        )
+        static_channel = blocks.multiply_matrix_cc(channel_matrix)
+        fft1 = fft.fft_vcc(fft_len, True, (), True)
+        fft2 = fft.fft_vcc(fft_len, True, (), True)
+        channel_est = digital.mimo_channel_estimator_vcvc(N, fft_len)
+
+        self.tb.connect(src, s2tagged_stream, ofdm_tx, (static_channel, 0), fft1, (channel_est, 0))
+        self.tb.connect((ofdm_tx, 1), (static_channel, 1), fft2, (channel_est, 1))
         self.tb.run ()
         # check data
 
