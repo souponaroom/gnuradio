e2bde94c786110a4ae92a079be3e4120584906dd
luca <luca.moritz.schmid@gmail.com>
2018-07-29 14:16:53 +0200

    mimo_ofdm_header_reader: QA test for a transmission without sync errors.

--- gr-digital/python/digital/qa_mimo_ofdm_header_reader_cc.py
+++ gr-digital/python/digital/qa_mimo_ofdm_header_reader_cc.py
@@ -50,23 +50,12 @@ class qa_mimo_ofdm_header_reader_cc (gr_unittest.TestCase):
             print 'Modulation not supported.'
             exit(1)
 
-    def test_001_t (self):
-        # Define test params.
-        packet_lengths = np.array([8, 2, 6])
-        data_length = np.sum(packet_lengths)
-        occupied_carriers = range(-26, -21) + range(-20, -7) + range(-6, 0) + range(1, 7) + range(8, 21) + range(22,27)
-        header_len = len(occupied_carriers)
-        print 'header len is ' + str(header_len)
-        bps_header = 1
-        bps_payload = 1
-        packet_len_tag_key = "packet_length"
-
-        # Constellations.
-        header_constellation = self._get_constellation(bps_header)
-        payload_constellation = self._get_constellation(bps_payload)
-
-        # Payload data.
-        data = np.random.randint(0, 100, data_length)
+    ''' Generate header and modulate payload data.'''
+    def generate_tx_data(self, data, packet_lengths,
+                         occupied_carriers,
+                         bps_header, bps_payload,
+                         packet_len_tag_key,
+                         header_constellation, payload_constellation):
         # Append stream tags to data.
         tags = []
         offset = 0
@@ -101,10 +90,22 @@ class qa_mimo_ofdm_header_reader_cc (gr_unittest.TestCase):
 
         payload_mod = digital.chunks_to_symbols_bc(payload_constellation.points())
 
-
         self.tb.connect(payload_src, payload_unpack, payload_mod, (header_payload_mux, 1))
         self.tb.connect(payload_src, header_gen)
+        sink = blocks.vector_sink_c()
+        self.tb.connect(header_payload_mux, sink)
+        self.tb.run()
+        return sink.data()
 
+    '''Accepts tagged stream data and reads/parses the header and demodulates the payload.'''
+    def demod(self, rx_data, rx_tags,
+              occupied_carriers,
+              bps_header, bps_payload,
+              header_constellation, payload_constellation
+              ):
+
+        rx_src = blocks.vector_source_c(data=rx_data,
+                                        tags=rx_tags)
         # Header reader.
         header_formatter_rx = digital.packet_header_ofdm(
             [occupied_carriers], 1,
@@ -114,35 +115,59 @@ class qa_mimo_ofdm_header_reader_cc (gr_unittest.TestCase):
             bps_header, bps_payload)
         header_reader = digital.mimo_ofdm_header_reader_cc(header_constellation.base(),
                                                            header_formatter_rx.formatter())
+        # Payload demodulator.
         payload_demod = digital.constellation_decoder_cb(payload_constellation.base())
         payload_pack = blocks.repack_bits_bb(bps_payload, 8, "", True)
-        sink = blocks.vector_sink_c()
-        self.tb.connect(header_payload_mux, sink)
+        sink_rx = blocks.vector_sink_b()
+        self.tb.connect(rx_src, header_reader, payload_demod, payload_pack, sink_rx)
         self.tb.run()
+        return sink_rx.data()
+
+    '''Test with 2 packets which follow each other directly.'''
+    def test_001_t (self):
+        # Define test params.
+        packet_lengths = np.array([4, 4])
+        data_length = np.sum(packet_lengths)
+        occupied_carriers = range(-26, -21) + range(-20, -7) + range(-6, 0) + range(1, 7) + range(8, 21) + range(22, 27)
+        header_len = len(occupied_carriers)
+        bps_header = 1
+        bps_payload = 1
+        packet_len_tag_key = "packet_length"
+
+        # Constellations.
+        header_constellation = self._get_constellation(bps_header)
+        payload_constellation = self._get_constellation(bps_payload)
+
+        # Payload data.
+        data = np.random.randint(0, 100, data_length)
+
+        # Generate tx data.
+        tx_data = self.generate_tx_data(data, packet_lengths,
+                                        occupied_carriers,
+                                        bps_header, bps_payload,
+                                        packet_len_tag_key,
+                                        header_constellation, payload_constellation)
+
+        # Manipulate data and tags
+        rx_data = tx_data
 
         # Read data from sink and add 'start' tags.
         rx_tags = []
         offset = 0
         for i in range(0, packet_lengths.size):
-            print 'offset = ' + str(offset)
             rx_tags.append(gr.tag_utils.python_to_tag((offset,
                                                        pmt.string_to_symbol("start"),
                                                        pmt.from_long(0))))
-            offset += packet_lengths[i]*8/bps_payload + header_len
-        rx_src = blocks.vector_source_c(data=sink.data(),
-                                        tags=rx_tags)
-        sink_rx = blocks.vector_sink_b()
+            offset += packet_lengths[i] * 8 / bps_payload + header_len
 
+        # Header and payload reader.
+        rx_data = self.demod(rx_data, rx_tags,
+                             occupied_carriers,
+                             bps_header, bps_payload,
+                             header_constellation, payload_constellation)
 
-        self.tb.connect(rx_src, header_reader, payload_demod, payload_pack, sink_rx)
-        self.tb.run ()
-        # check data
-
-        print 'restult'
-        print sink_rx.data()
-
-        print 'data'
-        print data
+        # Check if the expected result equals the actual result.
+        self.assertComplexTuplesAlmostEqual(data, rx_data, 2)
 
 if __name__ == '__main__':
     gr_unittest.run(qa_mimo_ofdm_header_reader_cc, "qa_mimo_ofdm_header_reader_cc.xml")
