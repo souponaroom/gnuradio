9ea826f6a510b593481bd8ac9775fdc1f6a960c5
luca <luca.moritz.schmid@gmail.com>
2019-03-25 13:57:43 +0100

    MIMO-OFDM: Integrate Maximum Ratio Combining into the MIMO-OFDM Transceiver.

--- gr-digital/python/digital/mimo_decoder_cc.py
+++ gr-digital/python/digital/mimo_decoder_cc.py
@@ -21,7 +21,7 @@
 # Boston, MA 02110-1301, USA.
 # 
 
-from gnuradio import gr
+from gnuradio import gr, blocks
 import digital_swig as digital
 from mimo import mimo_technique as mimo
 
@@ -65,9 +65,11 @@ class mimo_decoder_cc(gr.hier_block2):
         mimo_decoder = mimo_algorithm[mimo_technique]
         for i in range(0, N):
             self.connect((self, i), (mimo_decoder, i))
-        if mimo_technique == 'diversity_combining_SC':
+        if mimo_technique is mimo.RX_DIVERSITY_SC:
             equalizer = digital.vblast_decoder_cc_make(num_inputs=1, equalizer_type='ZF', vlen=vlen)
             self.connect(mimo_decoder, equalizer, self)
+        elif mimo_technique is mimo.RX_DIVERSITY_MRC:
+            self.connect(mimo_decoder, blocks.vector_to_stream(gr.sizeof_gr_complex, vlen), self)
         else:
             self.connect(mimo_decoder, self)
 
